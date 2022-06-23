aa4398c209aa442f793b1405a8518ab4c66efd5b
Sergei US <106164110+QA-USV@users.noreply.github.com>
2022-06-23 12:20:26 +0300

    Formatting

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate (int cost) {
+    public int calculate(int cost) {
         int bonusMiles = 1;
         int bonusMilesForEachSum = 20;
         return cost / bonusMilesForEachSum * bonusMiles;
